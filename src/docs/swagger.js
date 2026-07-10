9c444bc4751e8b34684035757387f2bb9049fa5f
Ganagani <ganaganiganesh5268@gmail.com>
2026-07-10 15:22:21 +0530

    update swagger domin

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -7,18 +7,47 @@ const swaggerSpec = swaggerJsdoc({
   apis: ['./src/routes/*.js', './src/controllers/*.js']
 });
 
+function getRequestOrigin(req) {
+  const forwardedProto = req.get('x-forwarded-proto');
+  const forwardedHost = req.get('x-forwarded-host');
+  const proto = forwardedProto ? forwardedProto.split(',')[0].trim() : req.protocol;
+  const host = forwardedHost ? forwardedHost.split(',')[0].trim() : req.get('host');
+  return `${proto}://${host}`;
+}
+
+function getSwaggerSpecForRequest(req) {
+  const origin = getRequestOrigin(req);
+  const localServer = { url: 'http://localhost:5000', description: 'Local development' };
+  const currentServer = { url: origin, description: 'Current deployment' };
+
+  return {
+    ...swaggerSpec,
+    servers: origin === localServer.url
+      ? [currentServer]
+      : [currentServer, localServer]
+  };
+}
+
 function mountSwagger(app) {
-  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  const swaggerUiOptions = {
     explorer: true,
-    customSiteTitle: 'HM Backend API Docs'
-  }));
+    customSiteTitle: 'HM Backend API Docs',
+    swaggerUrl: '/api/docs.json'
+  };
+
+  app.use('/api/docs', swaggerUi.serveFiles(null, swaggerUiOptions), (req, res, next) => {
+    return swaggerUi.setup(null, {
+      ...swaggerUiOptions
+    })(req, res, next);
+  });
 
   app.get('/api/docs.json', (req, res) => {
-    res.json(swaggerSpec);
+    res.json(getSwaggerSpecForRequest(req));
   });
 }
 
 module.exports = {
   mountSwagger,
-  swaggerSpec
+  swaggerSpec,
+  getSwaggerSpecForRequest
 };
